0179fe40713c25cc04f77f7aca65a0631442c75d
Anvesh G. Jhuboo <anvesh.jhuboo@gmail.com>
2023-12-19 16:25:26 -0500

    Refactor SidebarItem component to include icon

--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -3,23 +3,29 @@ import { twMerge } from "tailwind-merge";
 import Link from "next/link";
 
 interface SidebarItemProps {
-    icon: IconType;
-    label: string;
-    active?: boolean;
-    href: string;
+  icon: IconType;
+  label: string;
+  active?: boolean;
+  href: string;
 }
 
 const SidebarItem: React.FC<SidebarItemProps> = ({
-    icon,
-    label,
-    active,
-    href,
+  icon: Icon,
+  label,
+  active,
+  href,
 }) => {
-    return (
-        <Link href={href} className={twMerge(`flex flex-row h-auto items-center gap-x-4 font-medium cursor-pointer hover:text-white transition text-neutral-400 py-1`, active && "text-white")}>
-            Sidebar Item
-        </Link>
-    )
-}
+  return (
+    <Link
+      href={href}
+      className={twMerge(
+        `flex flex-row h-auto items-center gap-x-4 font-medium cursor-pointer hover:text-white transition text-neutral-400 py-1`,
+        active && "text-white"
+      )}
+    >
+        <Icon size={26} />
+    </Link>
+  );
+};
 
 export default SidebarItem;
