1a5734b4a0e042f98a0dbc6d89b1d2880b2a7629
Anvesh G. Jhuboo <anvesh.jhuboo@gmail.com>
2023-12-19 18:11:33 -0500

    Add image, name, and href props to ListItem component

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
           </h1>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grids-cols-4 gap-3 mt-4">
-          <ListItem />
+          <ListItem image="/images/liked.png" name="Liked Songs" href="liked" />
         </div>
       </Header>
     </div>
