4972cd32c156bf2147cadcd269e936f28bb7b7e8
Anvesh G. Jhuboo <anvesh.jhuboo@gmail.com>
2023-12-20 11:23:15 -0500

    Remove commented out code and reformat page.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -5,23 +5,17 @@ export default function Home() {
   return (
     <div className="text-white bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
     
-      {/* Header */}
       <Header>
         <div className="mb-2">
-
-          {/* Welcome back */}
           <h1 className="text-white text-3xl font-semibold">
             Welcome back
           </h1>
         </div>
-
-        {/* Liked Songs */}
         <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grids-cols-4 gap-3 mt-4">
           <ListItem image="/images/liked.png" name="Liked Songs" href="liked" />
         </div>
       </Header>
       
-      {/* Newest Songs */}
         <div className="mt-2 mb-7 px-6">
           <div className="flex justify-between items-center">
             <h1 className="text-white text-2xl font-semibold">
@@ -32,6 +26,7 @@ export default function Home() {
             List of Songs
           </div>
         </div>
+
     </div>
   )
 }
