77f84cb098ae759d40953b698c869d5d60f004ce
Anvesh G. Jhuboo <anvesh.jhuboo@gmail.com>
2023-12-20 12:06:29 -0500

    Update useUser error message

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -90,7 +90,7 @@ export const MyUserContextProvider = (props: Props) => {
 export const useUser = () => {
   const context = useContext(UserContext);
   if (context === undefined) {
-    throw new Error("useUser must be used within a UserContextProvider");
+    throw new Error("useUser must be used within a MyUserContextProvider");
   }
   return context;
 };
