68aad6496d6da1bbbe57a2b39ca81a865eb0c5e5
Anvesh G. Jhuboo <anvesh.jhuboo@gmail.com>
2023-12-20 12:25:40 -0500

    Add ModalProvider component to RootLayout

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  // This is a hack to prevent the "window is not defined" error during SSR.
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+  if (!isMounted) return null;
+
+  return <>Modals!</>;
+};
+
+export default ModalProvider;
