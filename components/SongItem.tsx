05a83d6548096992d4e0219c59be488d62275041
Anvesh G. Jhuboo <anvesh.jhuboo@gmail.com>
2023-12-20 22:06:21 -0500

    Update page layout and add image to SongItem component

--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import Image from "next/image";
+
 import { Song } from "@/types";
+import useLoadImage from "@/hooks/useLoadImage";
 
 interface SongItemProps {
   data: Song;
@@ -8,7 +11,23 @@ interface SongItemProps {
 }
 
 const SongItem: React.FC<SongItemProps> = ({ data, onClick }) => {
-  return <div>Song Item</div>;
+  const imagePath = useLoadImage(data);
+
+  return (
+    <div
+      onClick={() => onClick(data.id)}
+      className="relative group flex flex-col items-center justify-center rounded-lg overflow-hidden p-3 bg-neutral-800 hover:bg-neutral-700 cursor-pointer transition duration-200 ease-in-out"
+    >
+      <div className="relative aspect-square w-full h-full rounded-md overflow-hidden">
+        <Image
+          className="object-cover"
+          src={imagePath || "./images/liked.png"}
+          fill
+          alt="Song Image"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SongItem;
