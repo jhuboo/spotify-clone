e0926b97f31aac3ef550b0258837f4b58743599b
Anvesh G. Jhuboo <anvesh.jhuboo@gmail.com>
2023-12-21 09:33:17 -0500

    Implement search input with debounce

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -1,9 +1,31 @@
 "use client";
 
+import queryString from "query-string";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import useDebounce from "@/hooks/useDebounce";
+import Input from "@/components/Input";
+
 const SearchInput = () => {
-  // TODO: Implement search input
-  // NOTE: Add debounce to prevent spamming the server
-  return <div>Search Input</div>;
+  const router = useRouter();
+  const [value, setValue] = useState<string>("");
+  const debouncedValue = useDebounce<string>(value, 500);
+
+  useEffect(() => {
+    const query = { title: debouncedValue };
+    const url = queryString.stringifyUrl({ url: "/search", query });
+
+    router.push(url);
+  }, [debouncedValue, router]);
+
+  return (
+    <Input
+      placeholder="Search for a song"
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+    />
+  );
 };
 
 export default SearchInput;
