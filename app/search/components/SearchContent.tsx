31367b0c3b88e1be8d844a7adbe4f973795f763e
Anvesh G. Jhuboo <anvesh.jhuboo@gmail.com>
2023-12-21 09:48:47 -0500

    Refactor SearchContent component to improve layout and add TODO for Like button

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -17,12 +17,19 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
   }
 
   return (
-    <div>
+    <div className="flex flex-col gap-y-2 w-full px-6">
       {songs.map((song: Song) => (
-        <div key={song.id}>
-          <div>
-            <MediaItem data={song} />
+        <div
+          key={song.id}
+          className="flex items-center gap-x-4 w-full"
+        >
+          <div className="flex-1">
+            <MediaItem
+              data={song}
+              onClick={() => {}}
+            />
           </div>
+          {/* TODO: Add Like button here */}
         </div>
       ))}
     </div>
