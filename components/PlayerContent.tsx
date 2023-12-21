cfdd609d7caafceb1c602c4e64a3e36128113fe2
Anvesh G. Jhuboo <anvesh.jhuboo@gmail.com>
2023-12-21 14:16:21 -0500

    Add volume control icon to player

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -6,6 +6,7 @@ import LikeButton from "./LikeButton";
 
 import { BsPlayFill, BsPauseFill } from "react-icons/bs";
 import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
+import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
 
 interface PlayerContentProps {
   song: Song;
@@ -14,6 +15,11 @@ interface PlayerContentProps {
 
 const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
   const Icon = true ? BsPauseFill : BsPlayFill;
+  const VolumeIcon = true ? HiSpeakerXMark : HiSpeakerWave;
+
+  const handlePlay = () => {
+    console.log("play");
+  };
 
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 h-full">
@@ -28,7 +34,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
       {/* Mobile View */}
       <div className="flex md:hidden col-auto w-full justify-end items-center">
         <div
-          onClick={() => {}}
+          onClick={handlePlay}
           className="h-10 w-10 flex items-center justify-center rounded-full bg-white p-1 cursor-pointer"
         >
           <Icon
@@ -45,7 +51,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
           className="text-neutral-400 cursor-pointer hover:text-white transition"
         />
         <div
-          onClick={() => {}}
+          onClick={handlePlay}
           className="flex items-center justify-center h-10 w-10 rounded-full bg-white p-1 cursor-pointer"
         >
           <Icon
@@ -57,6 +63,16 @@ const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
           size={30}
           className="text-neutral-400 cursor-pointer hover:text-white transition"
         />
+      </div>
+
+      <div className="hidden md:flex w-full justify-end pr-2">
+        <div className="flex items-center gap-x-2 w-[120px]">
+          <VolumeIcon
+            size={30}
+            onClick={() => {}}
+            className="cursor-pointer"
+          />
+        </div>
       </div>
     </div>
   );
