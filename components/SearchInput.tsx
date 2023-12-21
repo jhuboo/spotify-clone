bc1f361c73f99abbd0d45429133075bff0593614
Anvesh G. Jhuboo <anvesh.jhuboo@gmail.com>
2023-12-21 09:21:06 -0500

    Add SearchInput component

---
+++ components/SearchInput.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+const SearchInput = () => {
+  // TODO: Implement search input
+  // NOTE: Add debounce to prevent spamming the server
+  return <div>Search Input</div>;
+};
+
+export default SearchInput;
