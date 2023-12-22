d64207c350370d8b586a9d3da1a58680b054470a
Anvesh G. Jhuboo <anvesh.jhuboo@gmail.com>
2023-12-21 19:55:35 -0500

    Commented out user authentication check in useOnPlay hook

--- hooks/useOnPlay.ts
+++ hooks/useOnPlay.ts
@@ -11,9 +11,9 @@ const useOnPlay = (songs: Song[]) => {
 
   const onPlay = (id: string) => {
     // If the user is not logged in, open the auth modal
-    if (!user) {
-      return authModal.onOpen();
-    }
+    // if (!user) {
+    //   return authModal.onOpen();
+    // }
 
     player.setId(id);
     player.setIds(songs.map((song) => song.id));
